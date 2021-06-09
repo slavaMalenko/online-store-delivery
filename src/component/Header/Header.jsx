74ef248b11ed944c6390d5a34628e5ab5aae9683
slavaMalenko <denouement20@yandex.ru>
2021-06-09 20:49:24 +0300

    working with react-router-dom

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoSvg from '../../assets/img/pizza-logo.svg';
 
@@ -9,15 +10,19 @@ function Header() {
     return (
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={logoSvg} alt="Pizza logo" />
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to="/">
+                    <div className="header__logo">
+                        <img width="38" src={logoSvg} alt="Pizza logo" />
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className="header__cart">
-                    <Button className="button--cart" />
+                    <Link to="/cart">
+                        <Button className="button--cart" />
+                    </Link>
                 </div>
             </div>
         </div>
