2e7b0c79dd14b85164cc61007500e38f609f5c4a
slavaMalenko <denouement20@yandex.ru>
2021-06-10 23:01:52 +0300

    pizza list reducer

---
+++ src/redux/redusers/pizzas.js
@@ -0,0 +1,19 @@
+const initialState = {
+    items: [],
+    isLoading: false,
+}
+
+const pizzas = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_PIZZAS':
+            return {
+                ...state,
+                items: action.payload,
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default pizzas;
