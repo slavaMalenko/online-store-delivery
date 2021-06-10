b6eef35319ebad80958a7f3c622cd9fe6fa5646d
slavaMalenko <denouement20@yandex.ru>
2021-06-10 21:46:05 +0300

    store creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/app.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import store from './redux/store'
+
+import './scss/app.scss';
 
 ReactDOM.render(
   <React.StrictMode>
