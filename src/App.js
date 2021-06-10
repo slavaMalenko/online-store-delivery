5a23d75f2d5a3ee249f12bb7d4bcf19e1cb5e0f1
slavaMalenko <denouement20@yandex.ru>
2021-06-11 00:16:14 +0300

    refactoring the app component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import axios from 'axios';
 import React from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './component/Header/Header';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 
+import { setPizzas } from './redux/actions/pizzas';
+import { setSortBy, setCategory } from './redux/actions/filters';
 
 
-function App() {
 
-  const [pizzas, setPizzas] = React.useState([]);
 
-  React.useEffect(() => {
+class App extends React.Component {
+
+  componentDidMount() {
     axios
       .get("http://localhost:3000/db.json")
       .then(response => {
-        setPizzas(response.data.pizzas)
+        this.props.setPizzas(response.data.pizzas)
       })
-  }, []);
+  }
 
-  return (
-    <div className="wrapper">
+  render() {
+    return (
+      <div className="wrapper">
 
-      <Header />
+        <Header />
 
-      <div className="content">
-        <Route path="/" exact render={() => <Home items={pizzas} />} />
-        <Route path="/cart" exact render={() => <Cart />} />
+        <div className="content">
+          <Route path="/" exact render={() => <Home items={this.props.pizzas} />} />
+          <Route path="/cart" exact render={() => <Cart />} />
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    pizzas: state.pizzas.items,
+    filters: state.filters,
+  }
+}
+
+export default connect(mapStateToProps, { setPizzas, setSortBy, setCategory })(App);
