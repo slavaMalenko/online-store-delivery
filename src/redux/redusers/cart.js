71905faab1efeb42f1f8f6e76bccf52576003bd4
slavaMalenko <denouement20@yandex.ru>
2021-06-17 20:01:11 +0300

    bug fix with adding new pizzas

--- src/redux/redusers/cart.js
+++ src/redux/redusers/cart.js
@@ -10,6 +10,7 @@ const cart = (state = initialState, action) => {
             return {
                 ...state,
                 items: {
+                    ...state.items,
                     [action.payload.id]: !state.items[action.payload.id]
                         ? [action.payload]
                         : [...state.items[action.payload.id], action.payload]
