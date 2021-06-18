838508db8bffed64bfb5d1cb72e49a42992ac5b5
slavaMalenko <denouement20@yandex.ru>
2021-06-19 02:18:43 +0300

    action to add and remove pizzas one by one

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -3,11 +3,21 @@ export const addPizzaToCart = (pizza) => ({
     payload: pizza,
 })
 
-export const clearCart = () => ({
-    type: 'CLEAR_CART',
+export const plusCartItem = (id) => ({
+    type: 'PLUS_CART_ITEM',
+    payload: id,
+})
+
+export const minusCartItem = (id) => ({
+    type: 'MINUS_CART_ITEM',
+    payload: id,
 })
 
 export const removeCartItem = (id) => ({
     type: 'REMOVE_CART_ITEM',
     payload: id,
+})
+
+export const clearCart = () => ({
+    type: 'CLEAR_CART',
 })
--- src/redux/redusers/cart.js
+++ src/redux/redusers/cart.js
@@ -48,6 +48,54 @@ const cart = (state = initialState, action) => {
             }
         }
 
+        case 'PLUS_CART_ITEM': {
+            const newObjItems = [
+                ...state.items[action.payload].items,
+                state.items[action.payload].items[0],
+            ];
+
+            const newItems = {
+                ...state.items,
+                [action.payload]: {
+                    items: newObjItems,
+                    totalPrice: getTotalPrice(newObjItems),
+                }
+            };
+
+            const totalCount = Object.keys(newItems).reduce((prev, key) => prev += newItems[key].items.length, 0);
+            const totalPrice = Object.keys(newItems).reduce((prev, key) => prev += newItems[key].totalPrice, 0);
+
+            return {
+                ...state,
+                items: newItems,
+                totalPrice,
+                totalCount,
+            }
+        }
+
+        case 'MINUS_CART_ITEM': {
+            const oldItems = state.items[action.payload].items;
+            const newObjItems = oldItems.length > 1 ? state.items[action.payload].items.slice(1) : oldItems;
+
+            const newItems = {
+                ...state.items,
+                [action.payload]: {
+                    items: newObjItems,
+                    totalPrice: getTotalPrice(newObjItems),
+                }
+            }
+
+            const totalCount = Object.keys(newItems).reduce((prev, key) => prev += newItems[key].items.length, 0);
+            const totalPrice = Object.keys(newItems).reduce((prev, key) => prev += newItems[key].totalPrice, 0);
+
+            return {
+                ...state,
+                items: newItems,
+                totalPrice,
+                totalCount,
+            }
+        }
+
         case 'CLEAR_CART':
             return {
                 items: {},
