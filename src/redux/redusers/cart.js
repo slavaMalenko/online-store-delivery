96f56fa645d46e4ee1b5b4247391f571857692ea
slavaMalenko <denouement20@yandex.ru>
2021-06-18 23:25:06 +0300

    development of the structure of the basket

--- src/redux/redusers/cart.js
+++ src/redux/redusers/cart.js
@@ -4,23 +4,31 @@ const initialState = {
     totalCount: 0,
 }
 
+const getTotalPrice = (arr) => arr.reduce((prev, item) => prev += item.price, 0);
+
 const cart = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_PIZZA_CART': {
+            const currentPizzaItems = !state.items[action.payload.id]
+                ? [action.payload]
+                : [...state.items[action.payload.id].items, action.payload]
+
             const newPizzas = {
                 ...state.items,
-                [action.payload.id]: !state.items[action.payload.id]
-                    ? [action.payload]
-                    : [...state.items[action.payload.id], action.payload]
+                [action.payload.id]: {
+                    items: currentPizzaItems,
+                    totalPrice: getTotalPrice(currentPizzaItems),
+                }
             }
 
-            const allArrPizzas = [].concat.apply([], Object.values(newPizzas))
+            const items = Object.values(newPizzas).map(item => item.items);
+            const allArrPizzas = [].concat.apply([], Object.values(items));
 
             return {
                 ...state,
                 items: newPizzas,
                 totalCount: allArrPizzas.length,
-                totalPrice: allArrPizzas.reduce((prev, item) => prev += item.price, 0)
+                totalPrice: getTotalPrice(allArrPizzas),
             }
         }
 
